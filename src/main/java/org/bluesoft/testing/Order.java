d68c80beac61add9d8a3d7af1c53744d374aaf78
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-04-22 13:14:55 +0200

    Adding assertThat to collection test.

--- src/main/java/org/bluesoft/testing/Order.java
+++ src/main/java/org/bluesoft/testing/Order.java
@@ -7,6 +7,15 @@ public class Order {
 
     private List<Meal> meals = new ArrayList<>();
 
-    
+    public void addMealToOrder(Meal meal){
+        this.meals.add(meal);
+    }
 
+    public void removeMealFromOrder(Meal meal){
+        this.meals.remove(meal);
+    }
+
+    List<Meal> getMeals() {
+        return meals;
+    }
 }
