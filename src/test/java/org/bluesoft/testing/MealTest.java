ae5770b88d7e1b87ad4e370d2b432f1e3805013b
Janusz <31537161+jstolorz@users.noreply.github.com>
2021-04-28 14:33:21 +0200

    Dynamic Tests @TestFactory

--- src/test/java/org/bluesoft/testing/MealTest.java
+++ src/test/java/org/bluesoft/testing/MealTest.java
@@ -1,13 +1,18 @@
 package org.bluesoft.testing;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -119,5 +124,38 @@ class MealTest {
 
         assertThat(price, lessThan(10));
     }
+
+    @TestFactory
+    Collection<DynamicTest> calculateMealPrices(){
+       //given
+       Order order = new Order();
+       order.addMealToOrder(new Meal(10,2,"Hamburger"));
+       order.addMealToOrder(new Meal(7,4,"Fries"));
+       order.addMealToOrder(new Meal(22,3,"Pizza"));
+
+       Collection<DynamicTest> dynamicTests = new ArrayList<>();
+
+       for (int i = 0; i < order.getMeals().size(); i++){
+           int price = order.getMeals().get(i).getPrice();
+           int quantity = order.getMeals().get(i).getQuantity();
+
+           Executable executable = () -> {
+             assertThat(calculatePrice(price,quantity), lessThan(67));
+           };
+
+           String name = "Test name: " + i;
+
+           DynamicTest dynamicTest = DynamicTest.dynamicTest(name,executable);
+           dynamicTests.add(dynamicTest);
+       }
+
+       return dynamicTests;
+    }
+
+    private int calculatePrice(int price, int quantity){
+        return price * quantity;
+    }
+
+
 
 }
